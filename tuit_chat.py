975f88c091e912033f892f8473ef7ce6cecbc67c
aniversarioperu <aniversarioperu1@gmail.com>
2013-10-27 01:02:29 +0300

    Fixed to reduce number of calls
    to twitter API. It was doing one call per user. Now it does only one call
    everything the script is run. This will save calls to avoid hitting API limit.

--- tuit_chat.py
+++ tuit_chat.py
@@ -14,23 +14,31 @@ import lukebot
 debug = 0;
 
 saved_tuits_file = "/data/projects/aniversario_peru/saved_tuits.txt"
+mentions_file = "/data/projects/aniversario_peru/mentions.csv"
 
 # t update "mi primer tuit"
 cmd2 = '/usr/local/bin/t set active AniversarioPeru'
 p = subprocess.check_call(cmd2, shell=True);
+
+def get_mentions():
+        cmd = "/usr/local/bin/t mentions -l -c"
+        p = subprocess.check_output(cmd, shell=True);
+        f = open(mentions_file, "w")
+        f.write(p)
+        f.close()
 
 def get_last_tuit(user):
     if debug:
         cmd = "cat debug_tuits.txt | grep -i " + user + " | head -n 1"
         p = subprocess.check_output(cmd, shell=True);
     else:
-        cmd = "/usr/local/bin/t mentions -l -c | grep -i " + user + " | head -n 1"
+        cmd = "cat " + mentions_file + " | grep -i " + user + " | head -n 1"
         p = subprocess.check_output(cmd, shell=True);
     if len(p) > 0:
         p_ = p.split(",")
         tuit_id = p_[0]
         tuit_date = p_[1]
-        message = p.split(',"')[1]
+        message = p_[3]
         message = message.replace('"', '')
         message = re.sub("\s*@AniversarioPeru\s*", "", message, re.I)
         return {
@@ -69,6 +77,8 @@ def reply(tuit):
 users = [
         "addTrollUser"
         ]
+
+get_mentions()
 for user in users:
     tuit = get_last_tuit(user)
     if tuit != "none":
